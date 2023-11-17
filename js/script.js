dbe291319a65d0bfff7dddcc2dabe118c8510a12
gaetano-94 <gaetano.panico1994@gmail.com>
2023-11-17 14:23:48 +0100

    fix: campo minato

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 'use strict';
 
 /*
+UTILITY
+*/
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+/*
 FUNZIONI
 */
 function myCreateElement(tag, className, content) {
@@ -35,6 +42,7 @@ function createTable(mainElement, cellNumber) {
   const cells = Math.sqrt(cellNumber);
 
   const fragment = document.createDocumentFragment();
+
   for (let i = 1; i <= cellNumber; i++) {
     const myElement = myCreateElement('div', 'cell', i);
     myElement.style.width = `calc(100% / ${cells})`;
@@ -44,16 +52,80 @@ function createTable(mainElement, cellNumber) {
   mainElement.append(fragment);
 }
 
+/*
+generateBombs: genera le bombe del gioco
+*/
+function generateBombs(num, numLimit) {
+  const bombs = [];
+  while (bombs.length < num) {
+    const currentNumber = getRndInteger(1, numLimit);
+
+    if (!bombs.includes(currentNumber)) {
+      bombs.push(currentNumber);
+    }
+  }
+
+  return bombs;
+}
+
+/*
+generateBombs: genera le bombe del gioco
+*/
+
+function gameLogic(board, cellNumber) {
+  const score = [];
+  let play = true;
+  const bombNumber = 16;
+  const bombs = generateBombs(bombNumber, cellNumber);
+  const message = document.querySelector('.game-status');
+  board.addEventListener('click', function (event) {
+    if (!event.target.classList.contains('cell')) return;
+    if (!play) return;
+
+    const currentElement = event.target;
+    const cellValue = parseInt(currentElement.innerHTML);
+
+    if (bombs.includes(cellValue)) {
+      //ho calpestato una bomba
+
+      currentElement.style.backgroundColor = 'red';
+
+      play = false;
+    } else {
+      currentElement.style.backgroundColor = 'blue';
+
+      if (!score.includes(cellValue)) {
+        score.push(cellValue);
+      }
+
+      if (score.length === cellNumber - bombs.length) {
+        message.innerHTML = `Hai vinto: ${score.length}`;
+      }
+    }
+  });
+}
+
 function campoMinato() {
+  resetGameFn();
   const table = document.querySelector('.table');
-  let level = 3;
+  const level = parseInt(document.getElementById('select-level').value);
   const cellNumber = setCellNumber(level);
+
   createTable(table, cellNumber);
+  gameLogic(table, cellNumber);
+}
+
+function resetGameFn() {
+  const board = document.querySelector('.table');
+  board.innerHTML = '';
 }
 
 /*
 VARIABILI GAME
 */
 
-const generaGriglia = document.getElementById('generaGriglia');
-generaGriglia.addEventListener('click', campoMinato);
+const startGame = document.getElementById('game-start');
+const resetGame = document.getElementById('game-reset');
+
+startGame.addEventListener('click', campoMinato);
+resetGame.addEventListener('click', resetGameFn);
